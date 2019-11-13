d9f84cf35dded6d99ad94a5655be163e975904d6
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-13 09:50:21 +0530

    Initial file for ex

---
+++ jquerytest.js
@@ -0,0 +1,5 @@
+$("div").css("backgroundColor","purple");
+$(".highlight").css("width","200px");
+$("#third").css("border", "2px solid orange");
+var div1 = $("div")[0];
+div1.css("text-color", "pink");
